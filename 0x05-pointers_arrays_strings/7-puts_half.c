6bf251fb2bf11d9466607ba34e36ccca466e29e3
Zamoo6 <a.ata.e.001@gmail.com>
2023-09-19 00:49:23 -0700

    puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,22 +12,22 @@
  */
 void puts_half(char *str)
 {
-    int length = strlen(str);
-    int start_index;
-
-    if (length % 2 == 0)
-    {
-        start_index = length / 2;
-    }
-    else
-    {
-        start_index = (length - 1) / 2;
-    }
-
-    for (; start_index < length; start_index++)
-    {
-        putchar(str[start_index]);
-    }
-
-    putchar('\n');
+	int length = strlen(str);
+	int start_index;
+	
+	if (length % 2 == 0)
+	{
+		start_index = length / 2;
+	}
+	else
+	{
+		start_index = (length - 1) / 2;
+	}
+	
+	for (; start_index < length; start_index++)
+	{
+		putchar(str[start_index]);
+	}
+	
+	putchar('\n');
 }
