52935b9ce159329edebe80362222b5b4c8404f3d
Zamoo6 <a.ata.e.001@gmail.com>
2023-09-19 00:53:30 -0700

    puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,7 +6,7 @@
  * @str: The input string.
  *
  * Description: If the number of characters in the string is odd, the function
- * prints the last n characters of the string, where n = (length_of_the_string - 1) / 2.
+ * prints the last n char of string, where n = (length_of_the_string - 1) / 2.
  *
  * Return: void.
  */
@@ -14,7 +14,6 @@ void puts_half(char *str)
 {
 	int length = strlen(str);
 	int start_index;
-	
 	if (length % 2 == 0)
 	{
 		start_index = length / 2;
@@ -23,11 +22,9 @@ void puts_half(char *str)
 	{
 		start_index = (length - 1) / 2;
 	}
-	
 	for (; start_index < length; start_index++)
 	{
 		putchar(str[start_index]);
 	}
-	
 	putchar('\n');
 }
