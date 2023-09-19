e8b59f87d4cf0b41cd134ab60faa0129082d7b70
Zamoo6 <a.ata.e.001@gmail.com>
2023-09-19 00:30:38 -0700

    puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * puts_half - Prints the second half of a string, followed by a new line.
+ * @str: The input string.
+ *
+ * Description: If the number of characters in the string is odd, the function
+ * prints the last n characters of the string, where n = (length_of_the_string - 1) / 2.
+ *
+ * Return: void.
+ */
+void puts_half(char *str)
+{
+    int length = strlen(str);
+    int start_index;
+
+    if (length % 2 == 0)
+    {
+        start_index = length / 2;
+    }
+    else
+    {
+        start_index = (length - 1) / 2;
+    }
+
+    for (; start_index < length; start_index++)
+    {
+        putchar(str[start_index]);
+    }
+
+    putchar('\n');
+}
