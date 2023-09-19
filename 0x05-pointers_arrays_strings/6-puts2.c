49fc8145f5cb08b156ef4ca32225bac87e2026b2
Zamoo6 <a.ata.e.001@gmail.com>
2023-09-18 23:49:27 -0700

    puts

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,47 +3,16 @@
  * rev_string - reverses a string
  * @s: string to be reversed
  */
-
-void puts2(char *str) {
-    int i = 0;
-
-    while (str[i] != '\0') {
-        if (i % 2 == 0) {
-            putchar(str[i]);
-        }
-        i++;
-    }
-
-    putchar('\n');
-}
-
-#include <stdio.h>
-#include "main.h"
-
-/**
- * rev_string - reverses a string
- * @s: string to be reversed
- */
-/*
-void rev_string(char *s)
+void puts2(char *str)
 {
-	char tmp;
-	int i, len, len1;
-
-	len = 0;
-	len1 = 0;
-
-	while (s[len] != '\0')
+	int i = 0;
+	while (str[i] != '\0')
 	{
-		len++;
-	}
-
-	len1 = len - 1;
-
-	for (i = 0; i < len / 2; i++)
+	if (i % 2 == 0)
 	{
-		tmp = s[i];
-		s[i] = s[len1];
-		s[len1--] = tmp;
+		putchar(str[i]);
 	}
-}*/
+	i++;
+	}
+	putchar('\n');
+}
