f434a5cf6dc7e618eb35b3793fad2e515b40ca57
Zamoo6 <a.ata.e.001@gmail.com>
2023-09-21 06:09:02 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,6 +6,11 @@
  * Return: Always 0 (Success)
  */
 
+int _putchar(char c)
+{
+	return write(1, &c, 1)
+}
+
 void print_alphabet(void)
 {
 	char letter = 'a';
@@ -22,5 +27,5 @@ void print_alphabet(void)
 int main(void)
 {
 	print_alphabet();
-	return (0);
+	return 0;
 }
