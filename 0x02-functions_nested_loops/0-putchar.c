c07919e0c1b7766e0bb8a3254d75df9cd3c6f30c
Zamoo6 <a.ata.e.001@gmail.com>
2023-09-21 03:00:08 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
 {
-    printf("_putchar\n");
-    return 0;
+	printf("_putchar\n");
+       	return 0;
 }
