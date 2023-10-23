90160b549af24fb431abc0c80c1525757e6e8da3
Zamoo6 <a.ata.e.001@gmail.com>
2023-10-22 21:53:13 -0700

    delete static libraries

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,8 @@
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(ing width, int height);
+void free_gird(int **grid, int height);
+char *argsostr(int ac, char **av);
+char **strtow(char *str);
