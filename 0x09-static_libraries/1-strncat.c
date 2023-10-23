bab2eaf8325ea6dfba7b4cddb10fc02123e6f4e9
Zamoo6 <a.ata.e.001@gmail.com>
2023-10-23 00:01:23 -0700

    libmy

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -6,15 +6,15 @@
  * @n: the limit of the concatenation
  * Return: a pointer to the resulting string dest
  */
-char *_strncat(char *dest, const char *src, int n)
+char* _strncat(char* dest, const char* src, int n)
 {
 	int index = 0, dest_len = 0;
-	{
+	
 	while (dest[index++])
 		dest_len++;
 
 	for (index = 0; src[index] && index < n; index++)
 		dest[dest_len++] = src[index];
-	}
+	
 	return (dest);
 }
