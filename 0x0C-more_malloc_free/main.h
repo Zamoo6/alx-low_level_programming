da335425a4790ef034eed4f8d07c5c98dd3c6347
Zamoo6 <a.ata.e.001@gmail.com>
2023-10-24 19:16:48 -0700

    main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,8 +1,32 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <limits.h>
+
+
 int _putchar(char c);
-char *create_array(unsigned int size, char c);
-char *_strdup(char *str);
-char *str_concat(char *s1, char *s2);
-int **alloc_grid(ing width, int height);
-void free_gird(int **grid, int height);
-char *argsostr(int ac, char **av);
-char **strtow(char *str);
+
+void *malloc_checked(unsigned int b);
+
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+void *_calloc(unsigned int nmemb, unsigned int size);
+
+int *array_range(int min, int max);
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+void errors(void);
+
+int _strlen(char *s);
+
+int is_digit(char *s);
+
+
+#endif
